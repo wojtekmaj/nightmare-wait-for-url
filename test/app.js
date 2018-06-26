7d3471521892b1e7d2d45cdbb2cd58751f45dfdf
Wojciech Maj <wojciech.maj@ocado.com>
2018-06-26 10:28:52 +0200

    Use express.static instead of serve-static

--- test/app.js
+++ test/app.js
@@ -1,9 +1,8 @@
 import express from 'express';
 import path from 'path';
-import serveStatic from 'serve-static';
 
 const app = express();
 
-app.use(serveStatic(path.resolve(__dirname, 'fixture')));
+app.use(express.static(path.join(__dirname, 'fixture')));
 
 export default app;
